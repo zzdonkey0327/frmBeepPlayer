6c4e8a71448c683c61e015c8de60d9491e9a6e7b
zzdonkey0327 <exonina2018027@gmail.com>
2026-05-13 22:19:31 +0800

    新增自動播放功能並優化電子琴 UI 響應式設計
    本次提交加入「自動播放」按鈕，可自動演奏小蜜蜂旋律，並將八個音階按鈕依序排列、標籤正確化。調整 UI 使其能隨視窗大小自適應，所有按鈕與主面板等比例縮放並動態調整字體。音階按鈕點擊時會播放對應音階，並暫時禁用避免重複觸發。程式碼結構亦優化，提升維護性與使用者體驗。

--- frmBeepPlayer/Form1.Designer.cs
+++ frmBeepPlayer/Form1.Designer.cs
@@ -29,14 +29,15 @@
         private void InitializeComponent()
         {
             this.palMain = new System.Windows.Forms.Panel();
-            this.btn1 = new System.Windows.Forms.Button();
-            this.btn2 = new System.Windows.Forms.Button();
-            this.btn3 = new System.Windows.Forms.Button();
-            this.btn4 = new System.Windows.Forms.Button();
-            this.btn5 = new System.Windows.Forms.Button();
-            this.btn6 = new System.Windows.Forms.Button();
-            this.btn7 = new System.Windows.Forms.Button();
             this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btnAutoPlay = new System.Windows.Forms.Button();
             this.palMain.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,10 +51,73 @@
             this.palMain.Controls.Add(this.btn3);
             this.palMain.Controls.Add(this.btn2);
             this.palMain.Controls.Add(this.btn1);
-            this.palMain.Location = new System.Drawing.Point(91, 117);
+            this.palMain.Location = new System.Drawing.Point(29, 29);
             this.palMain.Name = "palMain";
-            this.palMain.Size = new System.Drawing.Size(684, 321);
+            this.palMain.Size = new System.Drawing.Size(735, 321);
             this.palMain.TabIndex = 0;
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(608, 128);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(75, 23);
+            this.btn8.TabIndex = 7;
+            this.btn8.Text = "Do";
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(524, 128);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(75, 23);
+            this.btn7.TabIndex = 6;
+            this.btn7.Text = "Si";
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(440, 128);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(75, 23);
+            this.btn6.TabIndex = 5;
+            this.btn6.Text = "La";
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(356, 128);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 23);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "So";
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(272, 128);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 23);
+            this.btn4.TabIndex = 3;
+            this.btn4.Text = "Fa\r\n";
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(188, 128);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 23);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "Mi";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(104, 128);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "Re";
+            this.btn2.UseVisualStyleBackColor = true;
             // 
             // btn1
             // 
@@ -63,75 +127,24 @@
             this.btn1.TabIndex = 0;
             this.btn1.Text = "Do";
             this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
             // 
-            // btn2
+            // btnAutoPlay
             // 
-            this.btn2.Location = new System.Drawing.Point(95, 128);
-            this.btn2.Name = "btn2";
-            this.btn2.Size = new System.Drawing.Size(75, 23);
-            this.btn2.TabIndex = 1;
-            this.btn2.Text = "Re";
-            this.btn2.UseVisualStyleBackColor = true;
-            // 
-            // btn3
-            // 
-            this.btn3.Location = new System.Drawing.Point(170, 128);
-            this.btn3.Name = "btn3";
-            this.btn3.Size = new System.Drawing.Size(75, 23);
-            this.btn3.TabIndex = 2;
-            this.btn3.Text = "Mi";
-            this.btn3.UseVisualStyleBackColor = true;
-            // 
-            // btn4
-            // 
-            this.btn4.Location = new System.Drawing.Point(245, 128);
-            this.btn4.Name = "btn4";
-            this.btn4.Size = new System.Drawing.Size(75, 23);
-            this.btn4.TabIndex = 3;
-            this.btn4.Text = "button4";
-            this.btn4.UseVisualStyleBackColor = true;
-            // 
-            // btn5
-            // 
-            this.btn5.Location = new System.Drawing.Point(320, 128);
-            this.btn5.Name = "btn5";
-            this.btn5.Size = new System.Drawing.Size(75, 23);
-            this.btn5.TabIndex = 4;
-            this.btn5.Text = "button5";
-            this.btn5.UseVisualStyleBackColor = true;
-            // 
-            // btn6
-            // 
-            this.btn6.Location = new System.Drawing.Point(395, 128);
-            this.btn6.Name = "btn6";
-            this.btn6.Size = new System.Drawing.Size(75, 23);
-            this.btn6.TabIndex = 5;
-            this.btn6.Text = "button6";
-            this.btn6.UseVisualStyleBackColor = true;
-            // 
-            // btn7
-            // 
-            this.btn7.Location = new System.Drawing.Point(470, 128);
-            this.btn7.Name = "btn7";
-            this.btn7.Size = new System.Drawing.Size(75, 23);
-            this.btn7.TabIndex = 6;
-            this.btn7.Text = "button7";
-            this.btn7.UseVisualStyleBackColor = true;
-            // 
-            // btn8
-            // 
-            this.btn8.Location = new System.Drawing.Point(545, 128);
-            this.btn8.Name = "btn8";
-            this.btn8.Size = new System.Drawing.Size(75, 23);
-            this.btn8.TabIndex = 7;
-            this.btn8.Text = "button8";
-            this.btn8.UseVisualStyleBackColor = true;
+            this.btnAutoPlay.Location = new System.Drawing.Point(352, 356);
+            this.btnAutoPlay.Name = "btnAutoPlay";
+            this.btnAutoPlay.Size = new System.Drawing.Size(88, 40);
+            this.btnAutoPlay.TabIndex = 8;
+            this.btnAutoPlay.Text = "Play";
+            this.btnAutoPlay.UseVisualStyleBackColor = true;
+            this.btnAutoPlay.Click += new System.EventHandler(this.btnAutoPlay_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAutoPlay);
             this.Controls.Add(this.palMain);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -151,6 +164,7 @@
         private System.Windows.Forms.Button btn3;
         private System.Windows.Forms.Button btn2;
         private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btnAutoPlay;
     }
 }
 
--- frmBeepPlayer/Form1.cs
+++ frmBeepPlayer/Form1.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +8,141 @@ namespace frmBeepPlayer
 {
     public partial class Form1 : Form
     {
+        [DllImport("kernel32.dll")]
+        public static extern bool Beep(int frequency, int duration);
+
+        int[] freq = { 523, 587, 659, 698, 784, 880, 988, 1046 };
+
+        private SizeF originalClientSize;
+        private Rectangle originalPalMainRect;
+        private Rectangle originalAutoPlayRect;
+        private Rectangle[] originalBtnRects;
+        private Button[] buttons;
+
         public Form1()
         {
             InitializeComponent();
+            InitializeButton();
+
+            // 將電子琴鍵按鈕放大並重新排列
+            buttons = new Button[] { btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8 };
+            int padding = 5;
+            // 動態計算每個按鍵的寬度和高度，適應 palMain 的當前大小
+            int keyWidth = (palMain.Width - ((buttons.Length - 1) * padding)) / buttons.Length;
+            int keyHeight = palMain.Height > 40 ? palMain.Height - 40 : palMain.Height;
+
+            int startX = (palMain.Width - (buttons.Length * keyWidth + (buttons.Length - 1) * padding)) / 2;
+            if (startX < 0) startX = 0;
+            int startY = (palMain.Height - keyHeight) / 2;
+
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].Size = new Size(keyWidth, keyHeight);
+                buttons[i].Location = new Point(startX + i * (keyWidth + padding), startY);
+                buttons[i].TabStop = false; // 取消焦點，避免出現藍色框框
+            }
+
+            btnAutoPlay.TabStop = false; // 取消自動播放按鈕的焦點
+
+            // 記錄初始長寬比，以便在調整視窗大小時等比例縮放
+            originalClientSize = this.ClientSize;
+            originalPalMainRect = new Rectangle(palMain.Location, palMain.Size);
+            originalAutoPlayRect = new Rectangle(btnAutoPlay.Location, btnAutoPlay.Size);
+            originalBtnRects = new Rectangle[buttons.Length];
+
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                originalBtnRects[i] = new Rectangle(buttons[i].Location, buttons[i].Size);
+            }
+
+            // 綁定視窗大小改變事件
+            this.SizeChanged += frmBeePlayer_SizeChanged;
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            btn.Enabled = false;
+            Beep(freq[btn.TabIndex], 300);
+            btn.Enabled = true;
+        }
+
+        private void InitializeButton()
+        {
+            btn2.Click += btn1_Click;
+            btn3.Click += btn1_Click;
+            btn4.Click += btn1_Click;
+            btn5.Click += btn1_Click;
+            btn6.Click += btn1_Click;
+            btn7.Click += btn1_Click;
+            btn8.Click += btn1_Click;
+        }
+
+        private async void btnAutoPlay_Click(object sender, EventArgs e)
+        {
+            btnAutoPlay.Enabled = false;
+
+            // 小蜜蜂音符: 5 3 3, 4 2 2, 1 2 3 4 5 5 5 (對應陣列索引)
+            // 對應頻率 freq[] 索引: Do=0, Re=1, Mi=2, Fa=3, So=4, La=5, Si=6
+            int[] melody = { 4, 2, 2, -1, 3, 1, 1, -1, 0, 1, 2, 3, 4, 4, 4, -1 };
+            int duration = 300; 
+
+            Color originalColor = SystemColors.Control;
+            if (buttons.Length > 0) originalColor = Color.White;
+
+            await Task.Run(() =>
+            {
+                foreach (int note in melody)
+                {
+                    if (note == -1) // 休止符
+                    {
+                        System.Threading.Thread.Sleep(300);
+                    }
+                    else
+                    {
+                        // 透過 Invoke 在主執行緒更新 UI 呈現按下的效果
+                        this.Invoke(new Action(() => buttons[note].BackColor = Color.LightSkyBlue));
+
+                        Beep(freq[note], duration);
+
+                        // 恢復原本的按鈕顏色
+                        this.Invoke(new Action(() => buttons[note].BackColor = originalColor));
+
+                        System.Threading.Thread.Sleep(50); // 音符間隔
+                    }
+                }
+            });
+
+            btnAutoPlay.Enabled = true;
+        }
+
+        private void frmBeePlayer_SizeChanged(object sender, EventArgs e)
+        {
+            if (originalClientSize.Width == 0 || originalClientSize.Height == 0) return;
+
+            float xRatio = this.ClientSize.Width / originalClientSize.Width;
+            float yRatio = this.ClientSize.Height / originalClientSize.Height;
+
+            palMain.Location = new Point((int)(originalPalMainRect.X * xRatio), (int)(originalPalMainRect.Y * yRatio));
+            palMain.Size = new Size((int)(originalPalMainRect.Width * xRatio), (int)(originalPalMainRect.Height * yRatio));
+
+            btnAutoPlay.Location = new Point((int)(originalAutoPlayRect.X * xRatio), (int)(originalAutoPlayRect.Y * yRatio));
+            btnAutoPlay.Size = new Size((int)(originalAutoPlayRect.Width * xRatio), (int)(originalAutoPlayRect.Height * yRatio));
+            float autoPlayFontSize = 9f * Math.Min(xRatio, yRatio);
+            if (autoPlayFontSize > 1) btnAutoPlay.Font = new Font(btnAutoPlay.Font.FontFamily, autoPlayFontSize);
+
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].Location = new Point((int)(originalBtnRects[i].X * xRatio), (int)(originalBtnRects[i].Y * yRatio));
+                buttons[i].Size = new Size((int)(originalBtnRects[i].Width * xRatio), (int)(originalBtnRects[i].Height * yRatio));
+
+                // 動態調整字體大小
+                float newFontSize = 9f * Math.Min(xRatio, yRatio);
+                if (newFontSize > 1)
+                {
+                    buttons[i].Font = new Font(buttons[i].Font.FontFamily, newFontSize);
+                }
+            }
         }
     }
 }
